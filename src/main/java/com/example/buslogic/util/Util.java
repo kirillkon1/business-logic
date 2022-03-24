380ec78800c2eb3f3d99e16b4647f46b447c07e5
LeartAule <kirill.kondrashow@mail.ru>
2022-03-24 20:36:18 +0300

    cleaned code

--- src/main/java/com/example/buslogic/util/Util.java
+++ src/main/java/com/example/buslogic/util/Util.java
@@ -1,6 +1,6 @@
 package com.example.buslogic.util;
 
-import com.example.buslogic.model.User;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
